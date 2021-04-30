f57b006aa97a1c0c8e046a76f637409e9adab507
Pierre Cathé <pierre.cathe@gmailcom>
2021-04-30 22:47:17 +0200

    essai d'overlay pour voir les images en gros

--- msg.js
+++ msg.js
@@ -5,6 +5,16 @@ window.onload = function getMsg() {
       document.getElementById("msg_container").innerHTML = this.responseText;
     }
   };
-  xmlhttp.open("GET","get_messages.php",true);
+  xmlhttp.open("GET", "get_messages.php", true);
   xmlhttp.send();
+}
+
+function showBigImage(path) {
+  overlay = document.getElementById("overlay")
+  imagebox = document.getElementById("imagebox")
+  closeImagebox = document.getElementById("closeImagebox")
+  overlay.style.display = "block"
+  imagebox.style.display = "block"
+  closeImagebox.style.display = "block"
+  bigImage.src = path
 }
