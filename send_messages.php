cbce55a958f39fd5c814a20a05c253f7f90fe19b
Pierre Cathé <pierre.cathe@gmailcom>
2021-05-03 09:19:01 +0200

    tests

--- send_messages.php
+++ send_messages.php
@@ -6,14 +6,16 @@ $message = $_POST["message"];
 // get the temporary name that PHP gave to the uploaded file
 $image = $_FILES["image"]["tmp_name"];
 $new_filename = uniqid(rand(), true);
-move_uploaded_file($image, "user_images/" . $new_filename);
+$new_image = "user_images/" . $new_filename;
+move_uploaded_file($image, $new_image);
 $thumbnail = $_FILES["thumbnail"]["tmp_name"];
-move_uploaded_file($thumbnail, "user_images/thumbnails/t_" . $new_filename);
+$new_thumbnail = "user_images/thumbnails/t_" . $new_filename;
+move_uploaded_file($thumbnail, $new_thumbnail);
 
 include "db_connect.php";
 
-error_log("INSERT INTO messages VALUES('" . $name . "', '" . $message . "', '" . $image . "', '" . $thumbnail . ");");
-$sql="INSERT INTO messages VALUES('" . $name . "', '" . $message . "', '" . $image . "', '" . $thumbnail . ");";
+error_log("INSERT INTO messages VALUES('" . $name . "', '" . $message . "', '" . $new_image . "', '" . $new_thumbnail . ");");
+$sql="INSERT INTO messages VALUES('" . $name . "', '" . $message . "', '" . $new_image . "', '" . $new_thumbnail . ");";
 $result = mysqli_query($con,$sql);
 
 mysqli_close($con);
