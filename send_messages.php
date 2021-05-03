f9e921f6d00770168ea747579d041fe746758cd4
Pierre Cathé <pierre.cathe@gmailcom>
2021-05-03 08:51:55 +0200

    point virgule

--- send_messages.php
+++ send_messages.php
@@ -8,11 +8,11 @@ $image = $_FILES["image"]["tmp_name"];
 $new_filename = uniqid(rand(), true);
 move_uploaded_file($image, "user_images/" . $new_filename);
 $thumbnail = $_FILES["thumbnail"]["tmp_name"];
-move_uploaded_file($thumbnail, "user_images/thumbnails/t_" . $new_filename)
+move_uploaded_file($thumbnail, "user_images/thumbnails/t_" . $new_filename);
 
 include "db_connect.php";
 
-
+error_log("INSERT INTO messages VALUES('" . $name . "', '" . $message . "', '" . $image . "', '" . $thumbnail . ");");
 $sql="INSERT INTO messages VALUES('" . $name . "', '" . $message . "', '" . $image . "', '" . $thumbnail . ");";
 $result = mysqli_query($con,$sql);
 
