af401497581fa8048d58dbeffad5c196dae4afbe
Pierre Cathé <pierre.cathe@gmailcom>
2021-05-04 09:04:58 +0200

    debug

--- msg.js
+++ msg.js
@@ -2,6 +2,7 @@ function getMsg() {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
+      debugger;
       document.getElementById("msg_container").innerHTML = this.responseText;
     }
   };
