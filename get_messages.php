e0268f76a525946095fcc6f657487b5e625503d7
Pierre Cathé <pierre.cathe@gmailcom>
2021-05-05 08:45:34 +0200

    test width

--- get_messages.php
+++ get_messages.php
@@ -14,11 +14,13 @@ while($row = mysqli_fetch_array($result)) {
   echo "<div class='message'>";
   if (!is_null($row['image']))
   {
-    echo "<td><img class='msgimage' src='" . $row['thumbnail'] . "' onclick='showBigImage(\"" . $row['image'] . "\")'/></td>";
+    echo "<img class='msgimage' src='" . $row['thumbnail'] . "' onclick='showBigImage(\"" . $row['image'] . "\")'/>";
   }
+  echo "<div class='msgtext'>"
   echo "<p class='msgbody'>" . $row['message'] . "</p>";
   echo "<p class='msgname'>" . $row['name'] . "</p>";
   echo "</div>";
+  echo "</div>"
 }
 echo "</div>";
 mysqli_close($con);
