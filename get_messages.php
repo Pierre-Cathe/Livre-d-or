9f3866cd4800cc7c8b3c3187696a63a76f6e48e9
Pierre Cathé <pierre.cathe@gmailcom>
2021-08-02 21:32:32 +0200

    •

--- get_messages.php
+++ get_messages.php
@@ -60,7 +60,7 @@ while($row = mysqli_fetch_array($result)) {
       $month = "dec.";
       break;
   }
-  echo "<p class='msgname'>" . $row['name'] . ", <span class='timestamp'> " . date('j ', $time) . $month . " - " . date('G:i', $time) . "</span></p>";
+  echo "<p class='msgname'>" . $row['name'] . ", <span class='timestamp'> " . date('j ', $time) . $month . " • " . date('G:i', $time) . "</span></p>";
   echo "</div>";
   echo "</div>";
   echo "</div>";
